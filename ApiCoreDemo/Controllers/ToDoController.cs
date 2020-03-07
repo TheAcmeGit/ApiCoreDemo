7c1f9f8dd23b1b9011e5416646bae6f28d0863ac
KO <KO@DESKTOP-LNNLHCR>
2020-03-07 16:33:17 +0800

    添加项目文件。

---
+++ ApiCoreDemo/Controllers/ToDoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiCoreDemo.Controllers
+{
+    [Produces("application/json")]//支持此种相应类型
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+
+        private readonly ILogger _logger;
+
+        public ToDoController(ILogger<ToDoController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/ToDo
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.Log(LogLevel.Information, "日志测试");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ToDo/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ToDo
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ToDo/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Common.Startup.Extensions/SwashbuckleCustomerExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Common.Startup.Extensions
+{
+    public static class SwashbuckleCustomerExtensions
+    {
+        public static IServiceCollection AddCustomerSwashbuckle(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Email = "admin@qq.com",
+                        Name = "admin",
+                        Url = new Uri("https://www.admin.com")//网站连接
+                    },
+                    Description = "描述",
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                string str = System.AppDomain.CurrentDomain.BaseDirectory;
+
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlFile = $"{System.AppDomain.CurrentDomain.BaseDirectory+ System.AppDomain.CurrentDomain.FriendlyName}.xml";//添加接口描述文档
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath,true);//第一个参数为：接口文档地址，第二个参数为：是否展示控制器描述注释
+            });
+            return services;
+        }
+
+
+        public static IApplicationBuilder UseCustomerSwashbuckle(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            return app;
+        }
+    }
+}
